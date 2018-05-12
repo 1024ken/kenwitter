daa65158f3b6ae4849615e11c64aa0601f8293ec
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2018-05-12 15:34:36 +0900

    Overall update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,57 +1,57 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:edit, :update, :destroy]    
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
   def index
-   @tweets = Tweet.all
+    @tweets = Tweet.all
   end
-  
+
   def new
-   if params[:back]
-    @tweet = Tweet.new(tweets_params)
-   else
-    @tweet = Tweet.new
-   end
+    if params[:back]
+      @tweet = Tweet.new(tweets_params)
+    else
+      @tweet = Tweet.new
+    end
   end
-  
+
   def create
-   @tweet = Tweet.new(tweets_params)
-   if @tweet.save
-    redirect_to tweets_path, notice:"Tweetを作成しました!"
-   else
-    render "new"
-   end
+    @tweet = Tweet.new(tweets_params)
+    if @tweet.save
+      redirect_to tweets_path, notice:"Tweetを作成しました!"
+    else
+      render "new"
+    end
   end
-  
+
   def edit
-   @tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
-  
+
   def update
-   @tweet = Tweet.find(params[:id])
-   if @tweet.update(tweets_params)
-    redirect_to tweets_path, notice:"Tweetを更新しました!"
-   else
-    render "edit"
-   end
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweets_params)
+      redirect_to tweets_path, notice:"Tweetを更新しました!"
+    else
+      render "edit"
+    end
   end
-  
-  def destroy 
-   @tweet = Tweet.find(params[:id])
-   @tweet.destroy
-   redirect_to tweets_path, notice:"Tweetを削除しました!"
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path, notice:"Tweetを削除しました!"
   end
-  
+
   def confirm
-   @tweet = Tweet.new(tweets_params)
-   render :new if @tweet.invalid?
+    @tweet = Tweet.new(tweets_params)
+    render :new if @tweet.invalid?
   end
-  
- private
+
+  private
   def tweets_params
     params.require(:tweet).permit(:name, :title, :comment)
   end
-  
+
   def set_tweet
-   @tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
- end
+end
